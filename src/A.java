5ac32ff2855d6280d1db1d65c9794612acfe3f4a
fengyem <987690319@qq.com>
2022-04-17 21:36:02 +0800

    【20220417】 first Commit 新增修改

--- src/A.java
+++ src/A.java
@@ -4,6 +4,7 @@
  * @date 2022/4/17 20:57
  */
 public class A {
+    private  int b;
 
     private String a;
 }
---
+++ src/B.java
@@ -0,0 +1,7 @@
+/**
+ * @author lcj
+ * @version 1.0
+ * @date 2022/4/17 21:32
+ */
+public class B {
+}
